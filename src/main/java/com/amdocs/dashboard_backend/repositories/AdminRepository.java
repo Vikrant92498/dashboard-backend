da5810501c7c1bd3ba807580809505a0c9587de6
Vikrant92498 <vikrantkumar92498@gmail.com>
2025-01-01 00:47:12 +0530

    deleted unused files

--- src/main/java/com/amdocs/dashboard_backend/repositories/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.amdocs.dashboard_backend.repositories;
-
-import com.amdocs.dashboard_backend.models.Admin;
-import com.amdocs.dashboard_backend.models.AdminRes;
-
-public interface AdminRepository {
-    AdminRes findById(String id);
-}
